933424eac3a371f8d91ae5992b530ddf86f83ff3
unknown <reddyswathi590@gmail.com>
2018-10-21 17:58:09 +0530

    first commit

---
+++ FileCreate.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace FileIO
+{
+    class FileStreamExample
+    {
+        public static void Main(string[] args)
+        {
+            FileStream f = new FileStream(@"F:\ReadWrite\Demo3.txt", FileMode.OpenOrCreate);//creating file stream  
+            f.WriteByte(65);//writing byte into stream  
+            f.Close();//closing stream  
+            //WriteForFile();
+            ReadForFile();
+            UpdateForFile();
+            Console.ReadLine();
+            void WriteForFile()
+            {
+                StreamWriter writer = new StreamWriter(@"F:\ReadWrite\Demo1.txt");
+                writer.WriteLine("hello world how r u");
+
+                writer.Close();
+            }
+            void ReadForFile()
+            {
+                StreamReader reader = new StreamReader(@"F:\ReadWrite\Demo1.txt");
+                Console.WriteLine(reader.ReadToEnd());
+                reader.Close();
+            }
+            void UpdateForFile()
+            {
+                StreamReader reader = new StreamReader(@"F:\ReadWrite\Demo1.txt");
+                Console.WriteLine(reader.ReadToEnd());
+                reader.Close();
+            }
+        }
+    }
+}
---
+++ JavascriptSerialization/Program.cs
@@ -0,0 +1,26 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+
+namespace JavascriptSerialization
+{
+    public class Dictionary
+    {
+        //public static object JsonConvert { get; private set; }
+
+        static void Main(string[] args)
+        {
+            Dictionary d1 = new Dictionary();
+            Dictionary<string,int> points = new Dictionary<string,int>
+            {
+                { "swathi", 9001 },
+                { "kanna", 3474 },
+                { "chinna", 11926 }
+            };
+
+            string json = JsonConvert.SerializeObject(points, Formatting.Indented);
+
+            Console.WriteLine(json);
+        }
+    }
+}//Serialization to dictionary
